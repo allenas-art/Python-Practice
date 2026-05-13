cc74dd9da42f4021f1405d842ce786545dc0c0ba
Ash Allen <allenas@wsc.school.nz>
2026-05-14 10:29:28 +1200

    Continued on quiz and lesson 4

--- main.py
+++ main.py
@@ -1,13 +1,4 @@
-
-age = 5
-print("He is " + str(age))
-
-
-
-name = input("What is you'r name?")
-print("Hello", name + "!}")
-age = input(" How old are you?")
-print(name, "is", age)
-
+if 5 < 10:
+    pass
 
 
