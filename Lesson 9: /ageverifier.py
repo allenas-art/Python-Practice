c1577baeb5f8ae49b8cc930a400c4c6859a340bc
Ash Allen <allenas@wsc.school.nz>
2026-06-22 11:04:49 +1200

    Revision

--- Lesson 9: /ageverifier.py
+++ Lesson 9: /ageverifier.py
@@ -6,31 +6,36 @@
 
 # VARIABLES
 # TODO Create a variable for valid input and set it to false
-Valid set false
+valid_input = False
 # GET INPUT
 # TODO Start a loop while the input is invalid
-while validinput != "false"
+while valid_input == False:
     # TODO Ask the user for their age and save it
-age = input("What is your age? ")
+    age = input("What is your age? ")
     #TRY
     # TODO Create a try statement
-try:
-    int(age)
-
+    try:
         # TODO Change the input into an integer and resave it
+        age = int(age)
         # TODO Set the valid input variable to true
-
+        valid_input = True
     # FAIL TO CONVERT TO INTEGER
     # TODO Add an except statement
+    except:
     # TODO Tell the user their input was invalid
-
+        print("Error, Input Invalid")
 # Unindented = Loop has finished so the input must be valid now
 
 # CHECK AGE
 # TODO Check if they are older than 18 and tell them they have access if they are
+if age >= 18: 
+    print("Access granted")
 # TODO Check if they are older than 13 and tell them they have partial access if they are.
+elif age >= 13 and age < 18:
+    print ("Partial access granted")
 # TODO Otherwise tell them access has been denied
-
+else:
+    print("Access denied")
 
 # ===================================================================
 # EXTENSION
