33c0a5fbb2ce324574276c478c85157ea5d3af37
Ash Allen <allenas@wsc.school.nz>
2026-08-17 11:02:11 +1200

    Update main.py

--- main.py
+++ main.py
@@ -1,15 +1,3 @@
-fruit = [
-    ["apples", 52],
-    ["oranges", 47],
-    ["pears", 57],
-    ["bananas", 89],
-    ["strawberries", 32],
-    ["grapes", 68],
-    ["blueberries", 57],
-    ["pineapples", 50],
-    ["kiwifruit", 61],
-    ["watermelons", 30]
-]
 
 print(fruit[5][1])
 
